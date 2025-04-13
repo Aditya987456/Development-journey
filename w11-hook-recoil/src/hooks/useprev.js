69eb1dfe193efb9e31c0c8ac6abc6a02d0b49592
Aditya raj <adityaraj987456@gmail.com>
2025-04-13 20:54:00 +0530

    updating usePrev.js

--- w11-hook-recoil/src/hooks/useprev.js
+++ w11-hook-recoil/src/hooks/useprev.js
@@ -10,6 +10,10 @@ export const useprev=(value)=>{
 
     },[value])  // prev val only update when the value changes...
 
+
+//###$$$$$ here according to react nature firstly it will return the value currentVal that is old val then it goes in useEffect hook and update the 
+//          currentVal if the [value] changes....
+
     return currentVal.current;
 
 }
