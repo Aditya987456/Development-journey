2f2cf141e578ba20688e04dc51ffd229714df752
Aditya raj <adityaraj987456@gmail.com>
2025-05-05 23:39:29 +0530

    structuring file for components

--- tailwind-w13/src/App.jsx
+++ tailwind-w13/src/App.jsx
@@ -8,6 +8,8 @@ function App() {
 
   return (
     <>
+
+{/*     
      <div className='grid grid-cols-12 '> 
 
       <div className=' bg-green-300 col-span-12 text-center   sm:col-span-5'>hi there</div>
@@ -15,8 +17,37 @@ function App() {
       <div className='bg-pink-300 col-span-12 text-center    sm:col-span-2'>hi there</div>
 
      </div>
-     
+*/}
      {/* <div className='bg-green-400 sm:bg-yellow-300 md:bg-blue-700 lg:bg-red-600'>hi there</div> */}
+ 
+
+{/* creating a project...in tailwind */}
+
+<div className='flex justify-center min-h-screen'>
+
+  <div>Webinar.gg</div>
+
+  <div></div>
+
+  <div></div>
+
+  <div></div>
+
+  <div></div>
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
 
     </>
   )
