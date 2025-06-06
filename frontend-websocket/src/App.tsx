69171d7aa8b16fab37bbdaac14d0372630e3d4da
Aditya raj <adityaraj987456@gmail.com>
2025-06-06 23:42:36 +0530

    using websockets creating ping-pong application

---
+++ frontend-websocket/src/App.tsx
@@ -0,0 +1,52 @@
+
+import { useEffect, useRef, useState } from 'react'
+import './App.css'
+
+
+
+
+function App() {
+  
+
+const [ socket, setSocket ]=useState();
+const inputMessage=useRef();
+
+function SendMessage() {
+  if(!socket) return
+
+  
+  const messages=inputMessage.current.value;
+  //@ts-ignore
+  socket.send(messages)
+}
+
+
+
+//connecting to websocket --> on mounting only.
+
+useEffect( ()=>{
+
+  const ws=new WebSocket('ws://localhost:8080');
+  setSocket(ws)
+
+  ws.onmessage=( (ev)=>{
+    alert(ev.data)
+  })
+  
+},[])
+
+
+
+
+
+  return (
+    <>
+      
+      <input ref={inputMessage} type='text' placeholder="Enter message here" />
+      <button onClick={SendMessage} style={{margin:8}}>SEND</button>
+      
+    </>
+  )
+}
+
+export default App
--- websocket-w16/src/index.ts
+++ websocket-w16/src/index.ts
@@ -5,13 +5,22 @@ const wss=new WebSocketServer({ port:8080 });
 
 //event handler
 wss.on('connection', function(socket){
-    console.log('user connected')
-    setInterval(() => {
-        socket.send('hello from websocket')
-    }, 500);
 
-    //socket level pe not like on wss on socket level.
+     wss.on('error', console.error);
+    console.log('user connected')
+
+    // setInterval(() => {
+    //     socket.send('hello from websocket')
+    // }, 500);
+
+    //$$ socket level pe not like on wss on socket level.
+
+    //-------------------------********  ping-pong application..  *********-----------------------------------
     socket.on('message', (e)=>{
         console.log(e.toString())
+        
+        e.toString()==='ping'? socket.send('pong'): socket.send('hutiya ping send kar!')
     })
+
+
 })
