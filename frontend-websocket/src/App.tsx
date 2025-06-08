4665b4a93077fe75771f07adb7eeabe85a34d0b1
Aditya raj <adityaraj987456@gmail.com>
2025-06-08 22:40:35 +0530

    created a small room based chat application

--- frontend-websocket/src/App.tsx
+++ frontend-websocket/src/App.tsx
@@ -60,9 +60,100 @@ import './App.css'
 
 
 
-//*************-----------------   Week-16.2    -----------------*********************** */
+//*************-----------------   chat - app     -----------------*********************** */
 
 function App() {
+
+
+
+
+  const [messages, setMessages]=useState(['hello ji']);
+
+  const InputRef=useRef()
+
+  const wsRef=useRef()
+
+
+
+
+
+  //connect on mounting...
+  useEffect( ()=>{
+    const ws=new WebSocket('ws://localhost:8080');
+
+    ws.onmessage=(event)=>{
+      setMessages(m=>[...m, event.data])
+    }
+
+    wsRef.current=ws;
+
+
+    //----only after coonected with server....----
+    ws.onopen=()=>{
+      ws.send(JSON.stringify({
+        type:'join',
+        payload:{
+          roomId:'123'
+        }
+      }))
+    }
+
+  },[])
+
+
+
+  function InputContentMsg() {
+    const inputBoxValue=InputRef.current.value;
+
+    wsRef.current.send(JSON.stringify({
+      type:'chat',
+      payload:{
+        message:inputBoxValue
+      }
+    }))
+
+    
+  }
+
+
+
+
+
+
+  return(
+    
+    <div className='h-[100vh] bg-black'>
+
+        <div className='h-[88vh]'>
+
+            {messages.map(msg=>
+              <div className='p-8'>
+                <span className='bg-white rounded p-4'>
+                   {msg} </span>
+              </div>)}
+          
+        </div>
+
+
+
+
+
+        <div>
+          <span className='flex justify-center '>
+
+          <input ref={InputRef} className=' bg-violet-200 rounded py-1 px-20 text-black' placeholder='Enter your message here!'></input>
+
+          <button className='bg-violet-600 hover:bg-violet-900 p-4 rounded m-2 text-white font-xl'
+            onClick={ InputContentMsg }
+           >SEND MESSAGE</button>
+          </span>
+        </div>
+
+
+      
+    </div>
+  
+  )
   
 }
 
